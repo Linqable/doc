a5e9ff6a40a93780e407f42d5488b19f375e119b
Yuuki Wesp <kozyr@itsph.ru>
2018-07-14 05:33:19 +0300

    added ga and travis

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import App from "./app";
 import * as ReactDOM from "react-dom";
 import * as React from "react";
+import * as ReactGA from 'react-ga';
+ReactGA.initialize('UA-70421575-5');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 
 ReactDOM.render(
     <App />,
